8a8b36367a88fdd8a5eb967d508a9931162daddd
EMZEDI <77243080+EMZEDI@users.noreply.github.com>
2022-01-02 18:21:56 -0500

    sec thoughts - not complete

--- src/Solu.java
+++ src/Solu.java
@@ -7,20 +7,45 @@ public class Solu {
         int width = 0;
         int start = 0;
         int reverse = height.length - 1;
+
+        int mokMinH = 0;
+        int mokStart = 0;
+        int mokRev = height.length - 1;
+
+        int area = 0;
+
         // iterate thru the list and calculate the min height
         for (int counter = 0; counter < height.length; counter++) {
 
             // calc the min height
-            if (height[start] > height[reverse]) min_height = height[reverse];
-            else min_height = height[start];
-
+            if (height[start] > height[reverse]){
+                min_height = height[reverse];
+                mokMinH = height[reverse];
+            }
+            else if (height[start] < height[reverse]){
+                min_height = height[start];
+                mokMinH = height[start];
+            }
             if (height[counter] > min_height){
-                if (min_height == height[start]) start = counter;
-                else reverse = counter;
+                if (mokMinH == height[mokStart]) mokStart = counter;
+                else mokRev = counter;
+
+                if (height[mokStart] > height[mokRev]){
+                    mokMinH = height[mokRev];
+                }
+                else if (height[mokStart] < height[mokRev]){
+                    mokMinH = height[mokStart];
+                }
+                // now if the mokarea with the new stuff is greater than the last one, do the change
+                if (((mokRev - mokStart) * mokMinH) > area) {
+                    if (min_height == height[start]) start = counter;
+                    else reverse = counter;
+                }
             }
             width = reverse - start;
+            area = width * min_height;
         }
-        return width * min_height;
+        return area;
     }
 
     public static void main(String[] args) {
